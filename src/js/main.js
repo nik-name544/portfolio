a198315192b44dec2dfa080efefaadb655f7c77a
nik-name544 <revenkonik30@gmail.com>
2020-06-19 20:25:18 +0300

    portfolio-upd_v0.2

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
 
+    $('.portfolio-items__portfolio-item').hover(
+        function () {
+            $('.portfolio-items__img').toggleClass('on');
+            $('.portfolio-items__img-right').removeClass('on');
+            $('.portfolio-items__portfolio-item').toggleClass('on');
+        }
+    );
+
+    $('.portfolio-items__portfolio-item-right').hover(
+        function () {
+            $('.portfolio-items__img-right').toggleClass('on');
+            $('.portfolio-items__img').removeClass('on');
+        }
+    );
 });
